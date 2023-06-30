04a299e03d200814e9626262dbf325a6108470cb
Aju Vijayan <59303483+ajuvijayan97@users.noreply.github.com>
2023-06-30 23:35:43 +0530

    Update AmazonTest.java

--- src/page/AmazonTest.java
+++ src/page/AmazonTest.java
@@ -1,5 +1,29 @@
 package page;
 
-public class AmazonTest {
+//import org.openqa.selenium.By;
+import org.testng.annotations.Test;
 
+import test.BaseClass;
+
+public class AmazonTest extends BaseClass {
+
+    @Test
+    public void testAmazon() {
+        driver.get("https://www.amazon.in/");
+        driver.manage().window().maximize();
+        AmazonPage page = new AmazonPage(driver);
+        page.enterSearchText("Laptop");
+        page.clickSearchButton();
+        page.clickCartIcon();
+        page.clickSignInButton();
+        page.enterEmail("xyx@gmaa.com");
+        page.clickContinueButton();
+        driver.navigate().back();
+        driver.navigate().back();
+        page.clickDepartmentsMenu();
+        page.clickOpenMenuLink();
+        page.clickCloseMenuLink();
+        page.openTodaysDeals();
+        
+    }
 }
